b807813f8bc1d23ffdbe4e0bf1bab1eace6c90bf
zxak <zx1580065013@163.com>
2020-12-27 20:41:07 +0800

    .

--- src/main/java/dto/FlowUnit.java
+++ src/main/java/dto/FlowUnit.java
@@ -19,50 +19,47 @@ public class FlowUnit {
     private ParameterHandler outParamHandler;
     private Condition condition;
     private Service service;
-    public  Boolean isContinue;
+    public Boolean isContinue;
 
 
-    public static List<FlowUnit> load(){
-        List<FlowUnit>  flowUnits=new ArrayList <FlowUnit>();
-        List settings=new ArrayList();
-        for(Object obj:settings){
+    public static List<FlowUnit> load() {
+        List<FlowUnit> flowUnits = new ArrayList<FlowUnit>();
+        List settings = new ArrayList();
+        for (Object obj : settings) {
             flowUnits.add(init());
         }
-        return flowUnits ;
+        return flowUnits;
     }
 
 
-    private static FlowUnit init(){
+    private static FlowUnit init() {
 
-        FlowUnit unit=new FlowUnit();
-        unit.inParamHandler=null;
-        unit.outParamHandler=null;
-        unit.condition=null;
-        unit.service=null;
-        unit.isContinue=false;
+        FlowUnit unit = new FlowUnit();
+        unit.inParamHandler = null;
+        unit.outParamHandler = null;
+        unit.condition = null;
+        unit.service = null;
+        unit.isContinue = false;
         return unit;
     }
 
-    public WorkResult work(FlowContext context, Map <String, Object> param) {
+    public WorkResult work(FlowContext context, Map<String, Object> param) {
 
         WorkResult result = new WorkResult();
-        try{
+        try {
             if (this.condition.check()) {
-                Map <String, Object> inParam = inParamHandler.doParam(param);
-                Map <String, Object> tempParam = this.service.doService(context, inParam);
-                Map <String, Object> outParam = outParamHandler.doParam(tempParam);
+                Map<String, Object> inParam = inParamHandler.doParam(param);
+                Map<String, Object> tempParam = this.service.doService(context, inParam);
+                Map<String, Object> outParam = outParamHandler.doParam(tempParam);
                 result.nextParam = outParam;
             }
-        }catch (Exception e){
-            context.exceptionHandler.handleException(e,context);
-            result.isSucess=false;
+        } catch (Exception e) {
+            context.exceptionHandler.handleException(e, context);
+            result.isSucess = false;
         }
         return result;
 
     }
-
-
-
 
 
 }
--- src/main/java/main/Executor.java
+++ src/main/java/main/Executor.java
@@ -1,4 +1,4 @@
-package service;
+package main;
 
 import dto.FlowContext;
 import dto.FlowUnit;
